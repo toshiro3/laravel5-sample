a26924a79a85fcd8c5ece8e90847d6fc65b6fc00
toshiro3 <t.ooka.102@gmail.com>
2018-03-26 20:10:34 +0900

    composerによるデプロイ処理を追加

---
+++ deploy.php
@@ -0,0 +1,64 @@
+<?php
+namespace Deployer;
+
+require 'recipe/laravel.php';
+
+// Project name
+set('application', 'laravel5-sample');
+
+// Project repository
+set('repository', 'https://toshiro3@github.com/toshiro3/laravel5-sample.git');
+
+// [Optional] Allocate tty for git clone. Default value is false.
+set('git_tty', true); 
+
+// Shared files/dirs between deploys 
+add('shared_files', []);
+add('shared_dirs', []);
+
+// Writable dirs by web server 
+add('writable_dirs', []);
+set('allow_anonymous_stats', false);
+
+// Hosts
+
+//host('project.com')
+//    ->set('deploy_path', '~/{{application}}');    
+
+localhost()
+    ->set('deploy_path', '/home/ec2-user/{{application}}')
+    ->set('release_name', getenv('$DEPLOYMENT_ID'));
+
+// Tasks
+
+task('build', function () {
+    run('cd {{release_path}} && build');
+});
+
+desc('Deploy batch');
+task('deploy_batch', [
+    'deploy:info',
+    'deploy:prepare',
+    'deploy:lock',
+    'deploy:release',
+    //'deploy:update_code',
+    'deploy:shared',
+    'deploy:vendors',
+    'deploy:writable',
+    'artisan:storage:link',
+    'artisan:view:clear',
+    'artisan:cache:clear',
+    'artisan:config:cache',
+    'artisan:optimize',
+    'deploy:symlink',
+    'deploy:unlock',
+    'cleanup',
+]);
+
+// [Optional] if deploy fails automatically unlock.
+after('deploy:failed', 'deploy:unlock');
+
+// Migrate database before symlink new release.
+
+before('deploy:symlink', 'artisan:migrate');
+
--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -2,6 +2,9 @@
 
 deploy_path=/home/ec2-user/laravel5-sample
 
-mkdir -p $deploy_path/shared
+#mkdir -p $deploy_path/shared
+#mv $deploy_path/releases/tmp $deploy_path/releases/$DEPLOYMENT_ID
+#ln -nfs $deploy_path/releases/$DEPLOYMENT_ID $deploy_path/current
+
 mv $deploy_path/releases/tmp $deploy_path/releases/$DEPLOYMENT_ID
-ln -nfs $deploy_path/releases/$DEPLOYMENT_ID $deploy_path/current
+$deploy_path/releases/$DEPLOYMENT_ID/vendor/bin/dep deploy_batch
